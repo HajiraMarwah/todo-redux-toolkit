b40a4a507d3888c2b042753a9bc88e52d0c6d51b
Hajira Marwah <hajiranadia12@gmail.com>
2025-09-09 22:35:50 +0530

    added add todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import "./index.css"
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 // import './App.css'
 
 function App() {
   
   return (
     <>
-      <h1 className='bg-green-500'>Todo Redux Toolkit</h1>
-
+     <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
