df7cf0e8f08b8f24cd404b4573727bd9f4cfd4a2
Hajira Marwah <hajiranadia12@gmail.com>
2025-09-09 22:45:52 +0530

    added todo too

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import "./index.css"
+import './App.css'
 import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
-// import './App.css'
 
 function App() {
   
